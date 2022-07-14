e99c7b56c77733b8199a75fc987d253c18ed7709
Aleksey Krupsky <dachadybenci2@gmail.com>
2022-07-14 12:56:31 +0300

    WEEK 6. Fix swagger docs

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,26 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 import { cwd } from 'node:process';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
+
+const DEFAULT_PORT_NUMBER = 4000;
 
 async function bootstrap() {
-  const DEFAULT_PORT_NUMBER = 4000;
-
   dotenv.config({ path: `${cwd()}/.env` });
+
+  const app = await NestFactory.create(AppModule);
+
+  const swaggerBuffer = await readFile(`${cwd()}/doc/api.yaml`);
+  const swaggerDocument = parse(swaggerBuffer.toString());
+
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const serverPort = process.env.PORT ?? DEFAULT_PORT_NUMBER;
 
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   await app.listen(serverPort);
 }
 
